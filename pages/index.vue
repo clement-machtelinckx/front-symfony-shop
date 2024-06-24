d67e607bcd9d5d48bf0cd9dbb515f61e64ffb79a
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2024-06-24 12:42:21 +0200

    frist commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
+</script>
+
+<template>
+<h1>yoyoyoyo</h1>
+
+</template>
---
+++ pages/test/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+const route = useRoute();
+const { data: pokemon } = await useFetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+const { data: articles } = await useFetch(`https://localhost:8000/api/articles`);
+
+console.log(pokemon);
+</script>
+
+<template>
+
+    <div>
+        <h1>Pokemon</h1>
+        <ul>
+        <li v-for="p in pokemon.results" :key="p.name">
+            <NuxtLink :to="`/pokemon/${p.name}`">{{ p.name }}</NuxtLink>
+
+        </li>
+        </ul>
+    </div>
+    <div>
+        <h1>Articles</h1>
+        <ul>
+            <li v-for="a in articles.resultes" :key="a.name">
+                <p></p>
+            </li>
+
+        </ul>
+       
+    </div>
+</template>
