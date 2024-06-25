ce66732d079a0d1c9563c5831ef10a6b17f448d2
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2024-06-25 12:33:45 +0200

    :bug:stand by

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>
-    <h1>User profile</h1>
-  </div>
+    <div>
+      <h1>user profil</h1>
+      <form>
+        <label for="firstname">FirstName:</label>
+        <input type="text" id="firstname">
+        <br>
+        <label for="lastname">Password:</label>
+        <input type="text" id="lastname">
+        <br>
+        <br>
+        <label for="phone">Phone:</label>
+        <input type="number" id="phone">
+        <br>
+        <br>
+        <label for="country">Contry:</label>
+        <input type="text" id="country">
+        <br>
+        <button type="submit">Send</button>
+      </form>
+      <div>
+          {{ user.value.email }}
+      </div>
+
+    </div>
 </template>
   
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const user = ref({});
   async function getUser() {
     const token = localStorage.getItem('token');
 
@@ -12,9 +36,10 @@
       navigateTo('/user/connexion')
       return;
     }
+  
 
     try {
-      const response = await fetch('https://localhost:8000/api/users/me', {
+      const response = await fetch('https://localhost:8000/api/users/1', {
         headers: {
           'Authorization': `Bearer ${token}`
         }
@@ -31,6 +56,9 @@
       // Gérer les erreurs réseau ou autres erreurs
     }
   }
+  onMounted(() => {
+  getUser();
+});
 
 </script>
   
